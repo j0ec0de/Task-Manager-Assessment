ec282607c4849be882802836f072fcc7a34545d7
j0ec0de <joelanto386@gmail.com>
2025-09-17 23:19:08 +0530

    Added MongoDb config

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 // dotenv
 
@@ -8,9 +9,17 @@ const app = express();
 
 const PORT = process.env.PORT || 3000
 
-
 app.use(express.json());
 
+// testing api using postman
+
+app.get('/', (req, res) => {
+    res.send('Hello from Node');
+})
+
+
+
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server started running on port: ${PORT}`);
 })
